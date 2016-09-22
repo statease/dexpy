Fixed power syntax error and test answer precision.

dexpy/tests/test_power.py
@@ -2,7 +2,9 @@ from unittest import TestCase
 
 import os
 import math
+import dexpy.design
 import dexpy.power
+import dexpy.model
 import numpy as np
 
 class TestPower(TestCase):
@@ -32,4 +34,4 @@ class TestPower(TestCase):
 
         power = dexpy.power.power(model, X, 2, 0.05)
 
-        np.testing.assert_almost_equal([1, 49, 49, 28.9, 63.1, 63.1], power)
+        np.testing.assert_allclose(power, [0.2887584, 0.49002743118623, 0.49002743118623, 0.28875325867897, 0.63145653747073, 0.63145653747073], rtol=1e-4)