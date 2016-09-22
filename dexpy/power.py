5365330778d5473c53f53909c35c29d96797aa86
Hank Anderson <hank.p.anderson@gmail.com>
2016-09-22 00:02:04 -0500

    Implemented power for continuous variables.
    Doesn't match dex yet.

--- dexpy/power.py
+++ dexpy/power.py
@@ -1,4 +1,35 @@
+import numpy as np
+from scipy.stats import f
+from scipy.special import ncfdtr
 
-def power(model, model_matrix, signal, noise, alpha):
+def power(model, model_matrix, effect_size, alpha):
 
-    model_matrix = design.main_effects
+    X = np.array(model_matrix)
+    print(X.shape)
+    residual_df = X.shape[0] - X.shape[1]
+
+    XtXi = np.linalg.inv(np.dot(np.transpose(X), X))
+    non_centrality = 1 / np.diag(XtXi)
+
+    # pre-calculate crit value for 1 df, most common case
+    crit_value = f.ppf(1 -alpha, 1, residual_df)
+
+    power = []
+    for t in range(0, len(model.terms)):
+        nc = adjust_non_centrality(non_centrality[t], model.terms[t])
+        nc *= effect_size * effect_size / 4.0
+        p = ncfdtr(1, residual_df, nc, crit_value) * 100.0;
+        print(residual_df)
+        print(nc)
+        print(crit_value)
+        print(non_centrality[t])
+        print()
+        power.append(p)
+
+    return power
+
+def adjust_non_centrality(nc, term):
+    """Adjusts the non-centrality parameter for terms that aren't -1 to 1"""
+    if term.always_positive():
+        return nc * 4 # term goes from 0 to 1
+    return nc
--- dexpy/tests/test_power.py
+++ dexpy/tests/test_power.py
@@ -2,15 +2,14 @@ from unittest import TestCase
 
 import os
 import math
-import dexpy.design
-import dexpy.model
+import dexpy.power
 import numpy as np
 
-class TestModelMatrix(TestCase):
-    """Tests for generating a model matrix"""
+class TestPower(TestCase):
+    """Tests for calculating power."""
 
-    def test_quadratic_model(self):
-        """Test expanding a quadratic model in a rotatable ccd"""
+    def test_quadratic_power(self):
+        """Test power for a quadratic model in a rotatable ccd"""
 
         axial_pt = math.sqrt(2)
         factor_data = [
@@ -29,6 +28,8 @@ class TestModelMatrix(TestCase):
 
         design = dexpy.design.Design(factor_data, response_data)
         model = dexpy.model.LinearModel.from_string("1 + A + B + AB + A^2 + B^2")
-
         X = design.create_model_matrix(model)
-        np.testing.assert_almost_equal([1.0, axial_pt, 0.0, -0.0, pow(axial_pt, 2), 0.0], X[5])
+
+        power = dexpy.power.power(model, X, 2, 0.05)
+
+        np.testing.assert_almost_equal([1, 49, 49, 28.9, 63.1, 63.1], power)
