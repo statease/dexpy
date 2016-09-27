1c8ef1ead52db641e30921efd218eb09ddea0763
Hank Anderson <hank.p.anderson@gmail.com>
2016-09-26 23:14:55 -0500

    Fixed coverage syntax.

--- ci/circle_tests.sh
+++ ci/circle_tests.sh
@@ -1,5 +1,5 @@
 # need to switch to conda env in the same shell as the tests are run
 # but circle each command has its own environment
 source activate dexpy_env
-coverage setup.py test
+coverage run setup.py test
 coverage html -d $CIRCLE_ARTIFACTS
