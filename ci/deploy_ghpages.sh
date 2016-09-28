fb72137ed8b9f314e3a7c20134ceac1b469f799a
Hank Anderson <hank.p.anderson@gmail.com>
2016-09-27 21:18:23 -0500

    Moved gh-pages logic into a separate shell script.

---
+++ ci/deploy_ghpages.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+GH_PAGES_SOURCES = docs/source docs/Makefile
+
+git checkout gh-pages
+rm -rf docs/build docs/_sources docs/_static
+git rm -rf .
+git checkout master $(GH_PAGES_SOURCES) .gitignore
+git reset HEAD
+cd docs && make html
+mv -fv docs/build/html/* .
+rm -rf $(GH_PAGES_SOURCES) docs/build
+touch .nojekyll
+git add -A
+git rm .gitignore -f
+git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
