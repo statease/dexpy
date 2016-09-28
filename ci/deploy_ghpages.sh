b071302bae53f6687e200822b2f96515a19b13b8
Hank Anderson <hank.p.anderson@gmail.com>
2016-09-27 23:09:15 -0500

    Build sphinx source before checking out gh-pages.

--- ci/deploy_ghpages.sh
+++ ci/deploy_ghpages.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-GH_PAGES_SOURCES="docs/source docs/Makefile"
+set -e # exit on error
+
+# first generate the html, since checking out gh-pages will destroy the source
+cd docs && make html
+cd ..
+
+mkdir -p ../dexpy-html
+mv -fv docs/build/html/* ../dexpy-html
 
 # need to set user/email, get from env vars (will need to be set in circle)
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 
 git checkout gh-pages
-rm -rf docs/build docs/_sources docs/_static
 git rm -rf .
-git checkout master $GH_PAGES_SOURCES .gitignore
+git checkout master .gitignore
 git reset HEAD
-cd docs && make html
-mv -fv docs/build/html/* .
-rm -rf $GH_PAGES_SOURCES docs/build
+mv ../dexpy-html/* .
 touch .nojekyll
 git add -A
 git rm .gitignore -f
