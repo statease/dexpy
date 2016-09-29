3d956141b3e148f49101e93650fb7aae0adca8db
Hank Anderson <hank.p.anderson@gmail.com>
2016-09-29 17:28:06 +0000

    Moved conda install commands to a shell script.

--- ci/circle_tests.sh
+++ ci/circle_tests.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # need to switch to conda env in the same shell as the tests are run
 # but circle each command has its own environment
 
---
+++ ci/install_conda.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# these are duplicated in circle.yml
+CONDA_ROOT=${HOME}/.miniconda
+DEXPY_ENV_NAME=dexpy_env
+DEXPY_ENV_ROOT=${CONDA_ROOT}/envs/${DEXPY_ENV_NAME}
+DEXPY_DEPENDENCIES="numpy scipy matplotlib coverage sphinx sphinx_rtd_theme"
+
+if [[ ! -d ${CONDA_ROOT} ]]; then
+    echo "Installing Miniconda...";
+    wget --quiet https://repo.continuum.io/miniconda/Miniconda3-3.16.0-Linux-x86_64.sh
+    bash Miniconda3-3.16.0-Linux-x86_64.sh -bf -p ${CONDA_ROOT};
+fi
+
+if [ ! -d ${DEXPY_ENV_ROOT} ]; then
+    ${CONDA_ROOT}/bin/conda create -y -n ${DEXPY_ENV_NAME} ${DEXPY_DEPENDENCIES};
+else
+    ${CONDA_ROOT}/bin/conda install -y -n ${DEXPY_ENV_NAME} ${DEXPY_DEPENDENCIES};
+fi
