a1a2b4f9addcfd155ed268fd2da6b993596eb469
Hank Anderson <hank.p.anderson@gmail.com>
2016-09-29 07:25:55 -0500

    Added set -e to test script.

--- ci/circle_tests.sh
+++ ci/circle_tests.sh
@@ -1,5 +1,8 @@
 # need to switch to conda env in the same shell as the tests are run
 # but circle each command has its own environment
+
+set -e # exit on error
+
 source activate dexpy_env
 coverage run setup.py test
 coverage html -d $CIRCLE_ARTIFACTS
