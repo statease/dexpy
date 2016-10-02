2954fcd370d651b0a680f80a7fe86ad5a11752c8
Hank Anderson <hank.p.anderson@gmail.com>
2016-10-02 14:11:19 -0500

    Removed factor import from init.

--- dexpy/__init__.py
+++ dexpy/__init__.py
@@ -5,7 +5,6 @@ A python package for Design of Experiments (DOE).
 """
 
 from dexpy.design import *
-from dexpy.factor import *
 from dexpy.factorial import *
 from dexpy.model import *
 from dexpy.power import *
