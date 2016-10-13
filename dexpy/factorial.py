f2dd7eaf6f690dab10d96cc67cba7df78c55653f
Hank Anderson <hank.p.anderson@gmail.com>
2016-10-12 23:14:32 -0500

    Started on fractional factorial code.

--- dexpy/factorial.py
+++ dexpy/factorial.py
@@ -1,6 +1,7 @@
 """Functions to build factorial designs."""
 
 import itertools
+import pandas as pd
 from dexpy.design import Design
 
 def build_factorial(factor_count, run_count):
@@ -18,4 +19,11 @@ def build_factorial(factor_count, run_count):
     if run_count == 2 ** factor_count:
         for run in itertools.product([-1, 1], repeat=factor_count):
             factor_data.append(list(run))
+    else:
+        for run in itertools.product([-1, 1], repeat=factor_count-1):
+            factor_data.append(list(run))
+        factor_data = pd.DataFrame(factor_data)
+        generator_column = factor_data.product(axis=1).rename("gen")
+        factor_data = factor_data.join(generator_column)
+
     return Design(factor_data, [])
--- dexpy/tests/test_factorial.py
+++ dexpy/tests/test_factorial.py
@@ -5,8 +5,15 @@ from dexpy.factorial import build_factorial
 class TestFactorial(TestCase):
 
     def test_full(self):
-
+        """Tests a full two-level factorial."""
         factor_count = 3
         run_count = 8
         design = build_factorial(factor_count, run_count)
         self.assertEqual(8, design.runs)
+
+    def test_res_v(self):
+        """Tests a 2^(5-1) fractional factorial."""
+        factor_count = 5
+        run_count = 16
+        design = build_factorial(factor_count, run_count)
+        self.assertEqual(16, design.runs)
