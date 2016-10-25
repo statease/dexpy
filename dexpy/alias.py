64da7a0ee002a40b7dcf2a42f76087b9d72a07f8
Hank Anderson <hank.p.anderson@gmail.com>
2016-10-24 21:58:04 -0500

    Switch to using LU for removing rank-deficient columns in alias check.

--- dexpy/alias.py
+++ dexpy/alias.py
@@ -1,17 +1,13 @@
 
 import numpy as np
+import scipy as sp
 from patsy import dmatrix
 
 
 def aliases(design, model):
 
-    model_matrix = np.array(dmatrix(model, design))
-    unaliased_matrix = remove_aliased_columns(model_matrix)
-    [alias_coefs, _, _, _] = np.linalg.lstsq(unaliased_matrix, model_matrix)
+    model_matrix = dmatrix(model, design)
+    _, upper_matrix = sp.linalg.lu(model_matrix, permute_l=True)
+    unaliased_matrix = model_matrix[:, np.array(np.diagonal(upper_matrix) > np.finfo(float).eps)]
+    alias_coefs, _, _, _ = np.linalg.lstsq(unaliased_matrix, model_matrix)
     return alias_coefs
-
-
-def remove_aliased_columns(matrix):
-
-    [_, singular_values, _] = np.linalg.svd(matrix)
-    return matrix[:, np.array(singular_values > np.finfo(float).eps)]
