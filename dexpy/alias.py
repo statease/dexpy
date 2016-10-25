1f04127e63eab901108fed52fdb1e6a138695f3f
Hank Anderson <hank.p.anderson@gmail.com>
2016-10-25 10:33:31 -0500

    Added human-readable alias list.

--- dexpy/alias.py
+++ dexpy/alias.py
@@ -2,13 +2,50 @@
 import numpy as np
 import scipy as sp
 from patsy import dmatrix
+import logging
 
 
-def aliases(design, model):
+def alias_list(design, model):
+    """Returns a human-readable list of dependent model columns."""
 
-    model_matrix = dmatrix(model, design)
-    _, upper_matrix = sp.linalg.lu(model_matrix, permute_l=True)
+    model_matrix = dmatrix(model, design, return_type="dataframe")
+    logging.debug("model matrix:\n%s", model_matrix)
+    alias_coefs = alias_matrix(model_matrix)
+    logging.debug("alias matrix:\n%s", alias_coefs)
+    aliases = []
+    for r in range(alias_coefs.shape[0]):
+        alias_strings = []
+        for c in range(alias_coefs.shape[1]):
+            if c == r:
+                continue
+            if alias_coefs[r, c] > np.finfo(float).eps:
+                if abs(alias_coefs[r, c] - 1.0) > np.finfo(float).eps:
+                    alias_strings.append("{}*{}".format(alias_coefs[r, c], model_matrix.columns[c]))
+                else:
+                    alias_strings.append(model_matrix.columns[c])
+        # FIXME: this is incorrect, because the alias matrix does not include
+        #        columns that are aliased, so we need to accomodate missing columns
+        if len(alias_strings):
+            aliases.append("{} = {}".format(model_matrix.columns[r], " + ".join(alias_strings)))
+    return aliases
+
+
+def alias_matrix(model_matrix):
+    """Determines which columns of the model are dependent on other columns.
+
+    This is done by solving AX=B, where X is the full rank model matrix,
+    and B is all of the columns of the model matrix. The result is a matrix
+    of coefficients which represent to what degree a given column is
+    collinear with another column.
+    """
+
+    model_matrix = np.array(model_matrix)
+    # there is no requirement that the model matrix is full rank
+    # so first remove linearly dependent columns using LU decomp
+    _, _, upper_matrix = sp.linalg.lu(model_matrix)
+    logging.debug("upper:\n%s", upper_matrix)
     unaliased_matrix = model_matrix[:, np.array(abs(np.diagonal(upper_matrix)) > np.finfo(float).eps)]
+    logging.debug("unaliased:\n%s", unaliased_matrix)
 
     alias_coefs, _, _, _ = np.linalg.lstsq(unaliased_matrix, model_matrix)
     return alias_coefs
--- dexpy/tests/test_alias.py
+++ dexpy/tests/test_alias.py
@@ -1,12 +1,17 @@
 from unittest import TestCase
 
-from dexpy.alias import aliases
+from dexpy.alias import alias_matrix, alias_list
 import dexpy.design as design
 import pandas as pd
 import numpy as np
+from patsy import dmatrix
+import logging
 
 
 class TestAliases(TestCase):
+
+    def setUp(self):
+        logging.basicConfig(level=logging.DEBUG)
 
     def test_two_factors(self):
 
@@ -20,7 +25,9 @@ class TestAliases(TestCase):
 
         factor_data = pd.DataFrame(factor_data, columns=design.get_factor_names(len(factor_data[0])))
 
-        alias_coefs = aliases(factor_data, "A + B")
+        model_matrix = dmatrix("A + B", factor_data)
+        logging.debug(alias_list(factor_data, "A + B + A*B"))
+        alias_coefs = alias_matrix(model_matrix)
         answer = [[1, 0, 0], [0, 1, 1]]
 
         np.testing.assert_allclose(alias_coefs, answer, rtol=1e-4, atol=np.finfo(float).eps)
