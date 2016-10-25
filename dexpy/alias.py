caeeec36c34b1d5c2281c623f3fca1dc7a66c9c4
Hank Anderson <hank.p.anderson@gmail.com>
2016-10-25 10:01:22 -0500

    Fixed zero check in alias matrix code.

--- dexpy/alias.py
+++ dexpy/alias.py
@@ -8,6 +8,7 @@ def aliases(design, model):
 
     model_matrix = dmatrix(model, design)
     _, upper_matrix = sp.linalg.lu(model_matrix, permute_l=True)
-    unaliased_matrix = model_matrix[:, np.array(np.diagonal(upper_matrix) > np.finfo(float).eps)]
+    unaliased_matrix = model_matrix[:, np.array(abs(np.diagonal(upper_matrix)) > np.finfo(float).eps)]
+
     alias_coefs, _, _, _ = np.linalg.lstsq(unaliased_matrix, model_matrix)
     return alias_coefs
