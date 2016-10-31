a971f4b68adfc39e6106092de2e8e75485e36e77
Hank Anderson <hank.p.anderson@gmail.com>
2016-10-30 23:30:09 -0500

    Central composite alpha can now be specified by name.
    Options are rotatable, spherical, orthogonal (for a
    quadratic model), practical, and face centered.
    
    You can also specify the alpha directly.

--- dexpy/ccd.py
+++ dexpy/ccd.py
@@ -3,6 +3,8 @@ import dexpy.design as design
 from dexpy.factorial import build_full_factorial
 import pandas as pd
 import numpy as np
+import math
+import numbers
 
 
 def build_ccd(factor_count, alpha):
@@ -15,6 +17,8 @@ def build_ccd(factor_count, alpha):
     axial_runs = pd.DataFrame(0,
                               index=np.arange(0, axial_count),
                               columns=factor_names)
+
+    alpha = alpha_from_type(factor_count, alpha)
     axial_run = 0
     for f in range(factor_count):
         axial_runs.loc[axial_run][f] = -alpha
@@ -24,3 +28,33 @@ def build_ccd(factor_count, alpha):
     factor_data = factorial_runs.append(axial_runs)
 
     return factor_data
+
+
+def alpha_from_type(factor_count, type):
+
+    if isinstance(type, numbers.Real):
+        return type
+
+    # TODO: these should be parameters
+    star_reps = 1
+    star_center_points = 0
+    star_points = factor_count * 2 * star_reps
+    factorial_points = 2**factor_count
+    factorial_center_points = 0
+
+    if type == "rotatable":
+        return math.sqrt(math.sqrt(factorial_points / star_reps));
+    if type == "spherical":
+        return math.sqrt(factor_count)
+    if type == "orthogonal":
+        vroot = math.sqrt(factorial_points + star_points + star_center_points + factorial_center_points) - math.sqrt(factorial_points)
+        qNumerator = vroot * vroot * factorial_points
+        qDenominator = 4.0 * star_reps * star_reps
+        # alpha = [(VNf)/(4Rs^2)]^(1/4)
+        return math.sqrt(math.sqrt(qNumerator/qDenominator))
+    if type == "practical":
+        return math.sqrt(math.sqrt(factor_count))
+    if type == "face centered" or type == "facecentered" or type == "face":
+        return 1.0
+
+    raise ValueError("Didn't recognize alpha type '{}'!".format(type))
--- dexpy/tests/test_ccd.py
+++ dexpy/tests/test_ccd.py
@@ -1,19 +1,68 @@
 from unittest import TestCase
 
 from dexpy.ccd import build_ccd
+from dexpy.ccd import alpha_from_type
 import numpy as np
 import patsy
 
 class TestCentralComposite(TestCase):
 
     def test_fcd(self):
-        """Tests a simple 3 factor face-centered central composite design."""
+        """Tests a simple 2 factor face-centered central composite design."""
 
         ccd_data = build_ccd(2, 1.0)
-        x_matrix = patsy.dmatrix("A+B+A*B+pow(A,2)+pow(B,2)", ccd_data, return_type="dataframe")
+        x_matrix = patsy.dmatrix("A+B+A*B+pow(A,2)+pow(B,2)",
+                                 ccd_data,
+                                 return_type="dataframe")
 
         xtxi = np.linalg.inv(np.dot(x_matrix.T, x_matrix))
         det_xtxi = np.linalg.det(xtxi)
 
         answer_d = 4.340e-4
         self.assertAlmostEqual(answer_d, det_xtxi)
+
+
+class TestAlphaParsing(TestCase):
+
+    def test_custom(self):
+        """Tests setting a custom alpha value."""
+        self.assertAlmostEqual(1.31415, alpha_from_type(5, 1.31415))
+
+    def test_rotatable(self):
+        """Tests setting a rotatable alpha value."""
+        self.assertAlmostEqual(1.4142135623731, alpha_from_type(2, "rotatable"))
+        self.assertAlmostEqual(2.3784142300054, alpha_from_type(5, "rotatable"))
+        self.assertAlmostEqual(4.7568284600109, alpha_from_type(9, "rotatable"))
+
+    def test_spherical(self):
+        """Tests setting a spherical alpha value."""
+        self.assertAlmostEqual(1.4142135623731, alpha_from_type(2, "spherical"))
+        self.assertAlmostEqual(2.2360679774998, alpha_from_type(5, "spherical"))
+        self.assertAlmostEqual(3.0, alpha_from_type(9, "spherical"))
+
+    def test_orthogonal(self):
+        """Tests setting an orthogonal alpha value."""
+        self.assertAlmostEqual(0.91017972112445, alpha_from_type(2, "orthogonal"))
+        self.assertAlmostEqual(1.5265329278543, alpha_from_type(5, "orthogonal"))
+        self.assertAlmostEqual(2.1121386170409, alpha_from_type(9, "orthogonal"))
+
+    def test_practical(self):
+        """Tests setting a practical alpha value."""
+        self.assertAlmostEqual(1.1892071150027, alpha_from_type(2, "practical"))
+        self.assertAlmostEqual(1.4953487812212, alpha_from_type(5, "practical"))
+        self.assertAlmostEqual(1.7320508075689, alpha_from_type(9, "practical"))
+
+    def test_face_centered(self):
+        """Tests setting a face-centered alpha value."""
+        self.assertAlmostEqual(1.0, alpha_from_type(2, "face centered"))
+        self.assertAlmostEqual(1.0, alpha_from_type(5, "face centered"))
+        self.assertAlmostEqual(1.0, alpha_from_type(9, "face centered"))
+
+    def test_invalid(self):
+        caught_exception = False
+        try:
+            alpha_from_type(45, "beeeeep")
+        except ValueError:
+            caught_exception = True
+        self.assertTrue(caught_exception)
+
