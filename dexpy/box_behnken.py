86ada57bbce3f50f3626d01a28b4a29d0f6a02ff
Hank Anderson <hank.p.anderson@gmail.com>
2016-11-18 22:30:32 -0600

    Added csv files of box-benkhen designs up to 21 factors.

--- dexpy/box_behnken.py
+++ dexpy/box_behnken.py
@@ -1,9 +1,6 @@
-from dexpy.factorial import build_factorial
 import dexpy.design as design
 import pandas as pd
-import numpy as np
-import math
-import itertools
+import os
 
 
 def build_box_behnken(factor_count, center_points = 5):
@@ -31,29 +28,18 @@ def build_box_behnken(factor_count, center_points = 5):
     number of runs generated to be multiplied by the number of combinations of
     the categorical factor levels.
 
+    Box, G.E.P., and Behnken, D.W., "Some New Three Level Designs for the Study
+    of Quantitative Variables", Technometrics, 2, pp. 455-475, 1960.
+
     :param factor_count: The number of factors to build for.
     :type factor_count: int
     :param center_points: The number of center points to include in the design.
     :type center_points: integer
     """
 
-    # the construction involves creating a 2^2 factorial for each pair of
-    # factors, with all other factors set to 0
-    # so there are (factor_count choose 2) * (2^2) runs
-    factorial_runs = 4
-    run_count = center_points + factorial_runs * (math.factorial(factor_count) /
-                                     math.factorial(2) /
-                                     math.factorial(factor_count - 2))
-
     factor_names = design.get_factor_names(factor_count)
-    factor_data = pd.DataFrame(0, columns=factor_names, index=np.arange(0, run_count))
-
-    factorial = build_factorial(2, factorial_runs)
-    start = 0
-    for combo in itertools.combinations(factor_names, 2):
-        end = start + factorial_runs - 1
-        factor_data.loc[start:end, combo[0]] = factorial['A'].tolist()
-        factor_data.loc[start:end, combo[1]] = factorial['B'].tolist()
-        start += factorial_runs
+    csv_path = os.path.join(os.path.dirname(__file__), "data", "BB_{:02d}.csv".format(factor_count))
+    factor_data = pd.read_csv(csv_path)
+    factor_data.columns = factor_names
 
     return factor_data
