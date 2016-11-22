e9815d9c152128e0846542a66165872243dd7fc2
Hank Anderson <hank.p.anderson@gmail.com>
2016-11-21 23:36:26 -0600

    Added simplex centroid build.

--- dexpy/model.py
+++ dexpy/model.py
@@ -1,8 +1,10 @@
 
 
-def make_quadratic_model(factor_names):
+def make_quadratic_model(factor_names, include_squared = True):
     """Creates patsy formula representing a quadratic model."""
 
     interaction_model = "({})**2".format("+".join(factor_names))
+    if not include_squared:
+        return interaction_model
     squared_terms = "pow({}, 2)".format(",2)+pow(".join(factor_names))
     return "{}+{}".format(interaction_model, squared_terms)
---
+++ dexpy/simplex_centroid.py
@@ -0,0 +1,46 @@
+import dexpy.design as design
+import pandas as pd
+import numpy as np
+import math
+import itertools
+
+def build_simplex_centroid(factor_count):
+    """Builds a Simplex Centroid mixture design.
+
+    This mixture design can be used for 3 to 8 components. A simplex-centroid
+    design consists of all points that are equally weighted mixtures of 1 to q
+    components. Included are permutations of:
+
+        Pure blends: (1, 0, ...,0)
+
+        Binary blends: (1/2, 1/2, 0, ...,0)
+
+        Tertiary blends: (1/3, 1/3, 1/3, 0, ...,0)
+
+    and so on to the overall centroid: (1/q, 1/q, ..., 1/q).
+
+    This design differs from a simplex-lattice design. It cannot be used to
+    estimate the full cubic model, but can be used to estimate a special cubic
+    model.
+    """
+
+    run_count = 0
+    for i in range(1, factor_count+1):
+        run_count += (math.factorial(factor_count) /
+                      math.factorial(i) /
+                      math.factorial(factor_count - i))
+
+    factor_names = design.get_factor_names(factor_count)
+    factor_data = pd.DataFrame(0, columns=factor_names,
+                                  index=np.arange(0, run_count))
+
+
+    row = 0
+    for i in range(1, factor_count+1):
+        proportion = 1 / i
+        for combo in itertools.combinations(factor_names, i):
+            factor_data.loc[row, combo] = proportion
+            row += 1
+
+
+    return factor_data
