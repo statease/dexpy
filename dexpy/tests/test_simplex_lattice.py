3eba8f578e55acc41d8671c63ed56464cfffdf2a
Hank Anderson <hank.p.anderson@gmail.com>
2016-11-24 13:55:38 -0600

    Added tests for quadratic simplex lattice.

--- dexpy/tests/test_simplex_lattice.py
+++ dexpy/tests/test_simplex_lattice.py
@@ -4,14 +4,16 @@ from dexpy.eval import det_xtxi
 from dexpy.model import make_model, ModelOrder
 import numpy as np
 import patsy
+import math
 
 class TestSimplexLattice(TestCase):
 
     def test_linear(self):
+        """Checks the optimality of linear simplex lattice designs."""
         answer_d = [ 1 ] * 29
         actual_d = []
+        order = ModelOrder.linear
         for i in range(2, 31):
-            order = ModelOrder.linear
             design = build_simplex_lattice(i, order)
             model = "-1 + " + make_model(design.columns, order,
                                          include_powers=False)
@@ -19,5 +21,32 @@ class TestSimplexLattice(TestCase):
                                      design,
                                      return_type="dataframe")
             actual_d.append(det_xtxi(x_matrix, use_log=False))
+
+        np.testing.assert_allclose(answer_d, actual_d, rtol=1e-5)
+
+    def test_quadratic(self):
+        """Checks the optimality of quadratic simplex lattice designs."""
+        answer_d = [
+            2.772588722239781, 8.317766166719345, 16.63553233343869,
+            27.725887222397816, 41.58883083359672, 58.224363167035406,
+            77.63248422271388, 99.81319400063214, 124.76649250079016,
+            152.49237972318798, 182.99085566782557, 216.26192033470295,
+            252.30557372382012, 291.1218158351771, 332.7106466687738,
+            377.0720662246103, 424.2060745026866, 474.1126715030026,
+            526.7918572255585, 582.2436316703541, 640.4679948373896,
+            701.4649467266647, 765.2344873381797, 831.7766166719344,
+            901.091334727929, 973.1786415061633, 1048.0385370066374,
+            1125.6710212293513, 1206.076094174305,
+        ]
+        actual_d = []
+        order = ModelOrder.quadratic
+        for i in range(2, 31):
+            design = build_simplex_lattice(i, order)
+            model = "-1 + " + make_model(design.columns, order,
+                                         include_powers=False)
+            x_matrix = patsy.dmatrix(model,
+                                     design,
+                                     return_type="dataframe")
+            actual_d.append(det_xtxi(x_matrix, use_log=True))
 
         np.testing.assert_allclose(answer_d, actual_d, rtol=1e-5)
