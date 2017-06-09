38bbd64cb5e93d8f1da109dc6a6abbab699c32e1
Hank Anderson <hank.p.anderson@gmail.com>
2017-06-09 11:12:24 -0500

    Added 4 factor optimal design test with a linear model.

--- dexpy/tests/test_optimal.py
+++ dexpy/tests/test_optimal.py
@@ -22,6 +22,21 @@ class TestOptimal(TestCase):
 
         self.assertAlmostEqual(d, 3.73506e-3, delta=1e-3)
 
+    def test_four_fac_linear(self):
+        """Tests a 4 factor optimal design with a linear model.
+
+        This should select 5 corners of the hypercube.
+        """
+
+        optimal_data = build_optimal(4, ModelOrder.linear)
+
+        model = make_model(optimal_data.columns, ModelOrder.linear, True)
+        X = dmatrix(model, optimal_data)
+        XtXi = np.linalg.inv(np.dot(np.transpose(X), X))
+        d = np.linalg.det(XtXi)
+
+        self.assertAlmostEqual(d, 4.34028E-4, delta=1e-4)
+
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(SomeTest)
     unittest.TextTestRunner(verbosity=0).run(suite)
